fe18b33a7c91de6acb718e54eca43ac4c2416669
Blair <abrgroup@gmail.com>
2020-04-26 10:50:17 -0400

    STRETCH - added GETBYID and PUTBYID routes

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -14,6 +14,42 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const car = await db("cars")
+      .where({ id })
+      .first();
+
+    res.json(car);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const payload = {
+      vin: req.body.vin,
+      make: req.body.make,
+      model: req.body.model,
+      mileage: req.body.mileage,
+      transmission: req.body.transmission,
+      title: req.body.title
+    };
+
+    await db("cars")
+      .where("id", req.params.id)
+      .update(payload);
+    const updatedCar = await db("cars")
+      .where("id", req.params.id)
+      .first();
+    res.json(updatedCar);
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.post("/", async (req, res, next) => {
   try {
     const carData = req.body;
